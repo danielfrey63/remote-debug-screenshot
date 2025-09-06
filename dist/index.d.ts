857f7af4f1479a1de28af225a8770adc1ee5cde8
danielfrey63 <daniel.frey@xmatrix.ch>
2025-09-06 14:44:16 +0200

    fix: removed git ignore files

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { DebugScreenshotButton } from './DebugScreenshotButton';
-export type { DebugScreenshotButtonProps } from './DebugScreenshotButton';
--- dist/index.js
+++
@@ -1,1 +0,0 @@
-export { DebugScreenshotButton } from './DebugScreenshotButton';
